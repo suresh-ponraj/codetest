3a3583ece57976017e6b2749d5115a9c739ae567
Ken Miles <kenm@jumbointeractive.com>
2017-07-03 09:07:15 +1000

    Tidy

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 codedir='/usr/src/codetest'
 echo ">>> Compling code"
-#docker run --rm -e GOPATH=/usr/ -v "$PWD":"$codedir" -w "$codedir" golang:1.8 sh -c "env && go get -v ./... && GOOS=linux CGO_ENABLED=0 go build -a -ldflags '-s' -tags netgo -ldflags '-w' -v"
 docker run --rm -e GOPATH=/usr/ -v "$PWD":"$codedir" -w "$codedir" golang:1.8 sh -c "env && go get -v ./... && GOOS=linux go build -tags netgo -installsuffix netgo -v"
-#docker run --rm -e GOPATH=/usr/ -v "$PWD":"$codedir" -w "$codedir" golang:1.8 sh -c "env && go get -v ./... && go build -v"
 echo ">>> Building docker container"
 docker build -t codetest .
